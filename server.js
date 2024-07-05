6c46841be192d80c0dc9bd6e356abcebb93132cc
YGainey <yousef.gainey@gmail.com>
2024-07-05 13:55:07 +0100

    All tasks done with some basic styling

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const methodOverride = require("method-override");
+const path = require("path");
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ const app = express()
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method")); 
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
 
 //routes
 app.get('/', async (req, res) => {
@@ -29,7 +31,6 @@ app.get('/books', async (req, res) =>{
     res.render('./books/index.ejs', {Book: allBooks})
 })
 
-
 app.get('/books/new', async (req, res) =>{
     res.render('./books/new.ejs')
 })
@@ -40,16 +41,31 @@ app.get('/books/:bookID', async (req, res) =>{
     res.render('./books/show.ejs', {Book : foundBook})
 })
 
+//post
 app.post('/books', async (req, res) =>{
     const createdBook = await Book.create(req.body)
     res.redirect('/books/new')
 })
 
+//delete
 app.delete('/books/:bookID', async (req, res) =>{
     await Book.findByIdAndDelete(req.params.bookID)
     res.redirect('/books')
 })
 
+//update
+app.get('/books/:bookID/edit', async (req, res) =>{
+    const bookID = req.params.bookID
+    const foundBook = await Book.findById(bookID)
+    res.render('./books/edit.ejs', {Book : foundBook})
+})
+
+app.put('/books/:bookID', async (req, res) => {
+    const bookID = req.params.bookID
+    await Book.findByIdAndUpdate(bookID, req.body)
+    res.redirect(`/books/${bookID}`)
+})
+ 
 //server connect
 const connect = async () => {
     try {
